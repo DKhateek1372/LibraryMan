1bdd3c07562630f18bed90a90a970e2f197d757a
Deepak Dk <deepak.khateek@applaunch.io>
2020-10-03 18:46:12 +0530

    initally library system

--- src/container/books.test.js
+++ src/container/books.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { listItemData } from '../../data-for-testing';
 import books from './books';
 import { shallow } from 'enzyme';
-const {key,title, publisher, categories, description } = listItemData;
+const {key, title, publisher, categories, description } = listItemData;
 
 it('renders without crashing', () => {
   // eslint-disable-next-line no-unused-vars
